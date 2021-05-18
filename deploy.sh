a9295bce3b6af83c53a7906d44027b676779f73d
testmeiswrong2020 <76488865+testmeiswrong2020@users.noreply.github.com>
2021-05-19 02:55:41 +0800

    .

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,18 @@ npm run build
 # cd 到构建输出的目录下 
 cd dist
 
+# 部署到自定义域域名
+# echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
 
+# 部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:testmeiswrong2020/vue_project_20210407.git master:gh-pages
+git push -f git@github.com:testmeiswrong2020/vue_project_20210407.git main:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
