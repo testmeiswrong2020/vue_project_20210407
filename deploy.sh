e0dd0f570bb9c57e75af91115c670818ce75ef53
testmeiswrong2020 <76488865+testmeiswrong2020@users.noreply.github.com>
2021-05-20 01:36:51 +0800

    .

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:testmeiswrong2020/vue_project_20210407.git main:gh-pages
+git push -f git@github.com:testmeiswrong2020/vue_project_20210407.git master:gh-pages
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
