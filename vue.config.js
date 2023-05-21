43b9936abeac64e8d85c6fe37aa2ad07b6f210ae
testmeiswrong2020 <76488865+testmeiswrong2020@users.noreply.github.com>
2023-05-22 01:04:31 +0800

    .

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,25 @@
-const path = require('path');
-// 載入 prerender-spa-plugin (第一步)
-const PrerenderSPAPlugin = require('prerender-spa-plugin');
-// 載入 PuppeteerRenderer (第二步)
-const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
+// const path = require('path');
+// // 載入 prerender-spa-plugin (第一步)
+// const PrerenderSPAPlugin = require('prerender-spa-plugin');
+// // 載入 PuppeteerRenderer (第二步)
+// const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 
 module.exports = {
-  // publicPath: process.env.NODE_ENV === 'production'
-  //   ? '/vue_project_20210407/'
-  //   : '/',
-  configureWebpack(config) {
-    if (process.env.NODE_ENV === 'production') {
-      config.plugins.push(
-        // 創建實例 (第三步)
-        new PrerenderSPAPlugin({
-          staticDir: path.join(__dirname, 'dist'),
-          routes: ['/', '/home'],
-          renderer: new Renderer({
-            renderAfterDocumentEvent: 'render-event',
-          }),
-        })
-      );
-    }
-  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue_project_20210407/'
+    : '/',
+  // configureWebpack(config) {
+  //   if (process.env.NODE_ENV === 'production') {
+  //     config.plugins.push(
+  //       // 創建實例 (第三步)
+  //       new PrerenderSPAPlugin({
+  //         staticDir: path.join(__dirname, 'dist'),
+  //         routes: ['/', '/home'],
+  //         renderer: new Renderer({
+  //           renderAfterDocumentEvent: 'render-event',
+  //         }),
+  //       })
+  //     );
+  //   }
+  // },
 }
